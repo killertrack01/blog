c5442b159ebdc20f11091b69a702a3056950a7b2
kinkin130501 <66099811+kinkin130501@users.noreply.github.com>
2020-06-17 09:57:56 +0700

    Trang admin nha

---
+++ blog/resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!-- Lưu tại resources/views/admin/index.blade.php -->
+@extends('layout.layout')
+@section('title', 'admin index')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Welcome to Dashboard Admin</h1>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    
+    <!--End  Main content -->
+@endsection
